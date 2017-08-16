966ff69f988f466eaf6b436e3543820b092e9d00
Alex Chaplinsky <alchaplinsky@gmail.com>
2017-08-16 23:28:00 +0200

    Add free luts and add previews for cine and insta

--- javascripts/application.js
+++ javascripts/application.js
@@ -10,8 +10,15 @@ $(document).ready(function() {
     } else {
       $('.header').removeClass('sticked')
     }
-  });
+  })
 
+  $('.carousele li').click(function(event) {
+    var image = $('.preview img')
+    $('.carousele li').removeClass('current')
+    $(event.currentTarget).addClass('current')
+    var src = $(event.currentTarget).find('img').attr('src')
+    image.attr('src', src)
+  })
   date = new Date()
   document.getElementById('copyright-year').innerText = date.getFullYear()
 });
